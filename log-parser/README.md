9a79d99332999ee09694ed6f16eb9b0ca5ccff92
egee-irl <brian@egee.io>
2019-06-03 21:51:37 -0700

    removed

--- log-parser/README.md
+++
@@ -1,3 +0,0 @@
-# default
-
-Unsure what this microservice will be called, let alone what it will do. Until I do, it shall be called _Default_ after the band.
--- log-parser/lib/helpers.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Helpers
-  def self.install_server(game_server)
-    container_name = Lxc.create(game_server.name)
-    Steamcmd.install_server(container_name, game_server)
-    Systemd.install_server(container_name, game_server)
-  end
-end
