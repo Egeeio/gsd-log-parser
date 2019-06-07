b4ab295ef2ca14d8de2141fb983624c573b050a1
egee-irl <brian@egee.io>
2019-06-07 14:33:34 -0700

    added test

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -4,6 +4,9 @@ export default async function Parse(game: string = "minecraft") {
   const log = childProcess.execSync(`journalctl --since '15 seconds ago' --no-pager -u ${game}`).toString();
   const regex = /(?<=\bUUID\sof\splayer\s)(\w+)/;
   const found = log.match(regex);
-  if (found)
-    console.log(found[0]);
+  if (found) {
+    const player = found[0];
+    console.log(`player joined: ${player}`);
+    childProcess.execSync(`echo ${player} >> /tmp/players`);
+  }
 }
