7cd57aee52e23906496fdb38a4331416ef28322a
egee-irl <brian@egee.io>
2019-06-07 13:53:01 -0700

    added found logic

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -4,5 +4,6 @@ export default async function Parse(game: string = "minecraft") {
   const log = childProcess.execSync(`journalctl --since '15 seconds ago' --no-pager -u ${game}`).toString();
   const regex = /(?<=\bUUID\sof\splayer\s)(\w+)/;
   const found = log.match(regex);
-  console.log(found);
+  if (found)
+    console.log(found[0]);
 }
