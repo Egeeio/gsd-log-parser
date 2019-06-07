af93cfb398c8cd8698800788c7ee76bd9ac94ded
egee-irl <brian@egee.io>
2019-06-07 13:44:07 -0700

    added regex plumbing

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,6 +1,8 @@
 import childProcess from "child_process";
 
-export default async function Parse() {
-  const test = childProcess.execSync("journalctl --no-pager -u docker");
-  console.log(test.toString());
+export default async function Parse(game: string) {
+  const log = childProcess.execSync(`journalctl --since '15 seconds ago' --no-pager -u ${game}`).toString();
+  const regex = /(?<=\bUUID\sof\splayer\s)(\w+)/;
+  const found = log.match(regex);
+  console.log(found);
 }
