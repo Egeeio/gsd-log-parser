9eec40fdef620aa02883a897b3a5a3d2839907b4
egee-irl <brian@egee.io>
2019-06-07 13:50:03 -0700

    default var

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,6 +1,6 @@
 import childProcess from "child_process";
 
-export default async function Parse(game: string) {
+export default async function Parse(game: string = "minecraft") {
   const log = childProcess.execSync(`journalctl --since '15 seconds ago' --no-pager -u ${game}`).toString();
   const regex = /(?<=\bUUID\sof\splayer\s)(\w+)/;
   const found = log.match(regex);
