994769dba8a832fe2fcaceff4539144f073a4874
egee-irl <brian@egee.io>
2019-06-08 00:55:09 -0700

    added redis

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -1,12 +1,20 @@
 import childProcess from "child_process";
+import Redis from "ioredis";
 
 export default async function Parse(game: string) {
   const log = childProcess.execSync(`journalctl --since '60 seconds ago' --no-pager -u ${game}`).toString();
   const regex = /(?<=\bUUID\sof\splayer\s)(\w+)/;
   const found = log.match(regex);
+
   if (found) {
     const player = found[0];
+    const redis = new Redis({
+      host: process.env.IP!,
+      port: parseInt(process.env.PORT!, 10),
+    });
+    redis.set("minecraft", player);
     childProcess.execSync(`echo ${player} >> /tmp/players`);
-    console.log(`player joined: ${player}`);
+    const fromRedis = await redis.get("minecraft");
+    console.log(`player joined: ${fromRedis}`);
   }
 }
