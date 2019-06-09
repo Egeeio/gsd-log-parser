cdfe3d6b54744a295c212221830f8da0226ce8bb
egee-irl <brian@egee.io>
2019-06-08 20:29:35 -0700

    use setInterval for scheduling

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import Parse from "./lib/parser";
 
 const game = process.argv.pop()!;
 
-if (game)
-  Parse(game);
-else
+if (game) {
+  setInterval(() => {
+    Parse(game);
+  }, 60000);
+} else {
   console.error("Missing argument, aborting...");
+  process.exit(1);
+}
