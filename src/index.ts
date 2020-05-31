073ca2867c526c8d9359cdd9091b8fa96573e7ae
egee-irl <brian@egee.io>
2020-05-31 13:52:05 -0700

    fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,49 @@
-import childProcess from "child_process";
-import Redis from "ioredis";
+import childProcess from 'child_process'
+import Redis from 'ioredis'
 
-const regex = {
-  "7days": /Player '.*/,
-  "minecraft": /(?<=\bUUID\sof\splayer\s)(\w+)/,
-  "rust": /^.*joined from ip/m,
-};
-const connString = {
-  host: process.env.IP!,
-  port: parseInt(process.env.PORT!, 10),
-};
+const interval = 30000
+const game = process.argv[2]
 
-function Subscribe(game: string) {
-  const redis = new Redis(connString);
-  const publisher = new Redis(connString);
-  redis.subscribe(game, (err, count) => { // TODO: use or safely remove unused vars
-    setInterval(() => {
-      Publish(game, publisher);
-    }, parseInt(process.env.LOOP!, 10));
-  });
+if (game) {
+  Subscribe(game)
+} else {
+  console.error('Missing argument, aborting...')
+  process.exit(1)
 }
 
-async function Publish(game: string, publisher: Redis.Redis) {
-  const matched = await Parse(game); // TODO: Unsure if this needs to be async
+function Subscribe (game: string) {
+  const connectionStr = {
+    host: 'localhost',
+    port: 6379
+  }
+  const redis = new Redis(connectionStr)
+  const publisher = new Redis(connectionStr)
+  redis.subscribe(game, (_err, _count) => {
+    setInterval(() => {
+      Publish(game, publisher)
+    }, interval)
+  })
+}
+
+async function Publish (game: string, publisher: Redis.Redis) {
+  const matched = await Parse(game)
   if (matched) {
-    const player = matched[0];
-    await publisher.publish(game, player);
-    console.log(`Published ${player} in ${game}`);
+    const player = matched[0]
+    await publisher.publish(game, player)
+    console.log(`Published ${player} in ${game}`)
   }
 }
 
-async function Parse(game: string) {
-  const log = childProcess.execSync
-    (`journalctl --user --since '${parseInt(process.env.LOOP!, 10)}ms ago' --no-pager -u ${game}`).toString();
-  return log.match(regex[game]);
-}
-
-const game = process.argv.pop()!;
-
-if (game) {
-  Subscribe(game);
-} else {
-  console.error("Missing argument, aborting...");
-  process.exit(1);
+async function Parse (game: string) {
+  const regex = {
+    '7days': /Player '.*/,
+    minecraft: /(?<=\bUUID\sof\splayer\s)(\w+)/,
+    rust: /^.*joined from ip/m
+  }
+  try {
+    const log = childProcess.execSync(`journalctl --user --since '${interval}ms ago' --no-pager -u ${game}`).toString()
+    return log.match(regex[game])
+  } catch (error) {
+    console.log(error.message)
+  }
 }
