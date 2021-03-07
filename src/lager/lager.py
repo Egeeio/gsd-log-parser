cca39761e54b20910056dbe2fd43cdac6b546593
egee_irl <brian@egee.io>
2021-03-06 19:18:43 -0800

    refactored to use docker

--- src/lager/lager.py
+++ src/lager/lager.py
@@ -3,38 +3,23 @@ import re
 import redis
 import subprocess
 
-debug = os.getenv("LOGGER_DEBUG")
-interval = os.getenv("INTERVAL")
 redis_host = os.getenv("REDIS_HOST")
-redis_pass = os.getenv("REDIS_KEY")
 redis_port = os.getenv("REDIS_PORT")
 game_daemon = os.getenv("GAME")
 
-rdb = redis.Redis(host=redis_host, password=redis_pass, port=redis_port)
+rdb = redis.Redis(host=redis_host, port=redis_port)
 regex_dict = {
     'sdtd': r"Player '.*",
     'minecraft': r"(?<=\bUUID\sof\splayer\s)(\w+)",
     'rust': r"(.*]:)(.*)(with steamid)(.*joined\s)",
 }
 
-if debug:
-    file = open(f"./src/tests/{game_daemon}.txt")
-    log_slice = file.read().replace("\n", " ")
-    file.close()
-    log_stdout = str(log_slice)
-else:
-    log_slice = subprocess.check_output(
-        [
-            'journalctl',
-            '--since',
-            f'45s ago',
-            '--no-pager',
-            '-u',
-            game_daemon
-        ],
-        universal_newlines=True
-    )
-    log_stdout = log_slice
+log_slice = subprocess.check_output(
+    [ 'docker', 'logs', '--since', '60s', game_daemon ],
+    universal_newlines=True
+)
+log_stdout = log_slice
+
 try:
     regex = regex_dict[game_daemon]
     match = re.search(regex, log_stdout)
